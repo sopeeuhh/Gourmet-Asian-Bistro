5f6c1a9a3d8c57e80c3d5222a9a1383f48695753
sopeeuhh <soohiaaw@gmail.com>
2025-09-27 18:03:42 -0400

    added script for the menu tabs

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll(".tab-button");
+    const tabContents = document.querySelectorAll(".tab-content");
+
+
+    tabContents.forEach((tab, index) => {
+        tab.computedStyleMap.display = index === 0 ? "block" : "none";
+
+    });
+
+    tabs.forEach((tab, index) =>{
+        tab.addEventListener("click", () => {
+            tabs.forEach(btn => btn.classList.remove("active"));
+            tab.classList.add("active");
+
+            tabContents.forEach((content, i) => {
+                content.computedStyleMap.display = i ===index ? "block" : "none";
+            });
+        });
+    });
+});
